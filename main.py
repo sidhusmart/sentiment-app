925e8f5e2eed2210fbc07674b3a7f4fb5ee7fe03
sidhusmart <sidharth.ramachandran@gmail.com>
2020-09-08 21:18:29 +0200

    Adding sentiment detection

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 from typing import Optional
 from fastapi import Depends, FastAPI
 from pydantic import BaseModel
+import pickle
+from enum import Enum
+import preprocessing
 
 app = FastAPI()
 
+class Sentiment(Enum):
+    POSITIVE = 1
+    NEGATIVE = 0
+
 class Review(BaseModel):
-    reviewText: str
+    text: str
     reviewerID: Optional[str] = None
     asin: Optional[str] = None
-    sentiment: Optional[str] = None
+    sentiment: Optional[Sentiment] = None
 
     class Config:
         schema_extra = {
             "example": {
-                "reviewText": "This was a great purchase, saved me much time test!",
+                "reviewText": "This was a great purchase, saved me much time!",
                 "reviewerID": "A1VU337W6PKAR3",
                 "asin": "B00K0TIC56"
             }
         }
 
-@app.post("/api/v1/sentiment", response_model=Review)
-async def predict(review: Review):
-    sentiment_label = ["NEGATIVE", "POSITIVE"]
-    review.sentiment = sentiment_label[1]
-    return review
+def load_model():
+    try:
+      global model, vectorizer
+      model = pickle.load(open('models/sentiment_classification.pickle','rb'))
+      vectorizer = pickle.load(open('models/tfidf_vectorizer.pickle','rb'))
+      print ('Models have been loaded')
+    except Exception as e:
+      raise ValueError('No model here')
 
+@app.post("/api/v1/sentiment", response_model=Review)
+async def predict(review: Review, model = Depends(load_model())):
+    text_clean = preprocessing.clean(review.text)
+    text_tfidf = vectorizer.transform([text_clean])
+    sentiment = model.predict(text_tfidf)
+    review.sentiment = sentiment.item().name
+    return review
